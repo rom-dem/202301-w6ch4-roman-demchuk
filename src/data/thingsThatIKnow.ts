ebe32f4ed45598c157a505b833a6816ffd242aa9
Róman Demchuk <roman.demchuk@gmail.com>
2023-02-16 20:39:21 +0100

    Create a list of things that I know

---
+++ src/data/thingsThatIKnow.ts
@@ -0,0 +1,16 @@
+import { type Thing } from "../types";
+
+const thingsThatAyKnow: Thing[] = [
+  { id: 1, name: "MacOs is not that bad" },
+  { id: 2, name: "Football is boring" },
+  { id: 3, name: "Backend is not that bad yet" },
+  { id: 4, name: "Conitos are tasty" },
+  { id: 5, name: "Always keep your OS updated" },
+  { id: 6, name: "Don't throw paper towels in the loo" },
+  { id: 7, name: "Be kind" },
+  { id: 8, name: "Sleep a lot" },
+  { id: 9, name: "Walking is good for you" },
+  { id: 10, name: "Drinking water is a must" },
+];
+
+export default thingsThatAyKnow;
